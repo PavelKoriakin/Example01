59b1b3f210e48e2d2d49d428427e7b1eed5d92d7
Koriakin Pavel <polo_kor@mail.ru>
2023-02-12 19:53:23 +0300

    Exemple03.1

--- Example03/Program.cs
+++ Example03/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 35;
-int nubmerB = 5;
+﻿int numberA = new Random().Next(1, 10);  //1,2,3,4,5....//
+Console.WriteLine(numberA);
+int nubmerB = new Random().Next(1, 10);
+Console.WriteLine(nubmerB);
 int result = numberA + nubmerB;
 Console.WriteLine(result);
